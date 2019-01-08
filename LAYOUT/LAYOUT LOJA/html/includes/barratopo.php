b9c76c6e8e0ff953df7cc5cfd2d11f064e8ad918
JoaoPaccola <paccola31@gmail.com>
2019-01-08 15:06:16 -0200

    divisão da index - topo e barratopo
    divisão da index - topo e barratopo

---
+++ LAYOUT/LAYOUT LOJA/html/includes/barratopo.php
@@ -0,0 +1,78 @@
+<body>
+
+<!-- LOADER -->
+<div id="loader">
+  <div class="position-center-center">
+    <div class="ldr"></div>
+  </div>
+</div>
+
+<!-- Wrap -->
+<div id="wrap"> 
+  
+
+  
+    <!-- TOP Bar -->
+  <div class="top-bar">
+  	<div class="container">
+    
+    <!-- Curency -->
+    <div class="curency">
+    	
+        
+                      <select class="selectpicker">
+                        <option>USD</option>
+                        <option>PKR</option>
+                        <option>FR</option>
+                      </select>
+</div>
+
+
+<!-- Login Info -->
+    <div class="login-info">
+    	
+        
+                      <ul>
+                      	<li><a href="#.">LOGIN</a></li>
+                      <li><a href="#."> MY ACCOUNT          </a></li>
+                      <li><a href="#.">MY CART</a></li>         
+                      
+                       <!-- USER BASKET -->
+              <li class="dropdown user-basket"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"><i class="icon-basket-loaded"></i> </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <div class="media-left">
+                      <div class="cart-img"> <a href="#"> <img class="media-object img-responsive" src="images/cart-img-1.jpg" alt="..."> </a> </div>
+                    </div>
+                    <div class="media-body">
+                      <h6 class="media-heading">WOOD CHAIR</h6>
+                      <span class="price">129.00 USD</span> <span class="qty">QTY: 01</span> </div>
+                  </li>
+                  <li>
+                    <div class="media-left">
+                      <div class="cart-img"> <a href="#"> <img class="media-object img-responsive" src="images/cart-img-2.jpg" alt="..."> </a> </div>
+                    </div>
+                    <div class="media-body">
+                      <h6 class="media-heading">WOOD STOOL</h6>
+                      <span class="price">129.00 USD</span> <span class="qty">QTY: 01</span> </div>
+                  </li>
+                  <li>
+                    <h5 class="text-center">SUBTOTAL: 258.00 USD</h5>
+                  </li>
+                  <li class="margin-0">
+                    <div class="row">
+                      <div class="col-xs-6"> <a href="shopping-cart.html" class="btn">VIEW CART</a></div>
+                      <div class="col-xs-6 "> <a href="checkout.html" class="btn">CHECK OUT</a></div>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+                      
+                      
+                      </ul>
+</div>
+        
+    
+    </div>
+  
+  </div>
